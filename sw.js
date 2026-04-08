36cc728235939aa3e7cd1a08e752b635177be61a
coredigitaltools-collab <coredigital.tools@gmail.com>
2026-04-08 16:23:56 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='core-v4';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.add(self.location.href)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match(self.location.href)));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>r)))});
