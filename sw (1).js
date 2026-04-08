7bf4fe58c7d3ca9782eb23dbbcefdbf92c49cdcc
coredigitaltools-collab <coredigital.tools@gmail.com>
2026-04-08 15:46:59 +0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,42 @@
+const C = 'core-v5';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-384.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(C)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => r))
+  );
+});
