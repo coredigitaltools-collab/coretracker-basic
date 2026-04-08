ba2c41706cf18887bd8021d2022e236082a0be34
coredigitaltools-collab <coredigital.tools@gmail.com>
2026-04-08 13:35:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,4 @@
-const C = 'core-v6';
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(C).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]).catch(() => cache.add('./index.html'));
-    }).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(ks => Promise.all(
-      ks.filter(k => k !== C).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => {
-          const clone = res.clone();
-          caches.open(C).then(cache => cache.put(e.request, clone));
-          return res;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const C='core-v4';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.add(self.location.href)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match(self.location.href)));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>r)))});
